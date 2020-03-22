e551e380a9e98097205a9d002a8e63462667503a
Aliaksandr Shyrokau <aliaksandrs@google.com>
2020-03-22 23:36:28 +0300

    created generator file which does training and detection on the video

--- api/flask_app.py
+++ api/flask_app.py
@@ -1,18 +1,28 @@
-from flask import Flask, render_template, make_response, url_for
+
+import face_recognition
+import cv2
+import datetime
+
+import generator
+
+from flask import Flask, render_template, make_response, url_for, Response
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
 
+date = datetime.date.today()
+
 ''' For now we are using dummy data but then we will get it from the video-chat or database. '''
 interview_data = [
     {
         'interviewer': 'Epam Mahager',
-        'interviewee': 'First Candidate',
+        'interviewee': 'Firstname Lastname',
         'agenda': 'Data Analyst position',
-        'date_time': 'March 18, 2020',
-        'image_source': '/static/avatar.jpg'
+        'date_time': '{:%B %d, %Y}\n'.format(date),
+        'image_source': '/static/default.jpg',
+        'video_source': '/static/IMG_0909.MOV'
     }
 ]
 
@@ -27,9 +37,14 @@ class About(Resource):
         return make_response(render_template('about.html'))
 
 
+@app.route('/video_feed')
+def video_feed():
+    return Response(generator.gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
 api.add_resource(Home, '/')
 api.add_resource(About, '/about')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ api/generator.py
@@ -0,0 +1,77 @@
+import face_recognition
+import os
+import cv2
+
+
+KNOWN_FACES_DIR = 'api/known_faces'
+TOLERANCE = 0.6
+FRAME_THICKNESS = 3
+FONT_THICKNESS = 1
+MODEL = 'cnn'
+
+known_faces = []
+known_names = []
+
+for name in os.listdir(KNOWN_FACES_DIR):
+    try:
+        for filename in os.listdir(f'{KNOWN_FACES_DIR}/{name}'):
+            image = face_recognition.load_image_file(f"{KNOWN_FACES_DIR}/{name}/{filename}")
+            encoding = face_recognition.face_encodings(image)[0]
+            known_faces.append(encoding)
+            known_names.append(name)
+    except NotADirectoryError:
+        pass
+
+
+def gen():
+    # when you use a web camera
+    video = cv2.VideoCapture(0)
+
+    # when you want to use video file
+    # video = cv2.VideoCapture('api/static/IMG_0909.MOV')
+
+    # Read until video is completed
+    while(video.isOpened()):
+        # Find OpenCV version
+        (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+        if int(major_ver) < 3:
+            fps = video.get(cv2.cv.CV_CAP_PROP_FPS)
+        else:
+            fps = video.get(cv2.CAP_PROP_FPS)
+        num_frames = 60
+
+        for i in range(0, num_frames):
+            ret, image = video.read()
+
+        # when we are trying to detect a face on the images instead video
+        # ret, image = video.read()
+        locations = face_recognition.face_locations(
+            image, number_of_times_to_upsample=0, model=MODEL)
+        encodings = face_recognition.face_encodings(image,  locations)
+        for face_encoding, face_location in zip(encodings, locations):
+            results = face_recognition.compare_faces(known_faces, face_encoding, TOLERANCE)
+            match = None
+            if True in results:
+                match = known_names[results.index(True)]
+                top_left = (face_location[3], face_location[0] - 20)
+                bottom_right = (face_location[1], face_location[2] + 15)
+                color = [48, 10, 36]
+                cv2.rectangle(image, top_left, bottom_right, color, FRAME_THICKNESS)
+
+                top_left = (face_location[3], face_location[2] + 10)
+                bottom_right = (face_location[1], face_location[2] + 50)
+                cv2.rectangle(image, top_left, bottom_right, color, cv2.FILLED)
+                cv2.putText(image, match, (face_location[3] + 30, face_location[2] + 35),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (200, 200, 200), FONT_THICKNESS)
+
+        frame = cv2.imencode('.jpg', image)[1].tobytes()
+        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+        # when we are trying to detect a face on the images instead video
+        # cv2.imshow(filename, image)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+
+if __name__ == "__main__":
+    gen()
